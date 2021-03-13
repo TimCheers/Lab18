b0c38cb30d03b9a26d97b04bcb3e9c187f956ac6
Larra Padla <artem1978354@gmail.com>
2021-03-13 20:53:35 +0500

    1

---
+++ Lab18.2/Lab18.2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class staff
+{
+public:
+	staff(string name, string post, double pay);
+	staff();
+	~staff();
+	void setStaff(string name, string post, double pay)
+	{
+		this->name = name;
+		this->pay = pay;
+		this->post = post;
+	}
+	void showS()
+	{
+		cout << "\nname: " << name << "\npay: " << pay << "\npost: " << post << endl;
+	}
+private:
+	string name;
+	string post;
+	double pay;
+};
+staff::staff(string name, string post, double pay)
+{
+	this->name = name;
+	this->pay = pay;
+	this->post = post;
+} 
+staff::staff()
+{
+}
+staff::~staff()
+{
+}
+
+int main()
+{
+    system("chcp 1251>nul");
+}
+
