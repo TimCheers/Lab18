5ba7ecfb36c52cc33ca547bbd44a885cea8770c4
Larra Padla <artem1978354@gmail.com>
2021-03-27 20:46:23 +0500

    й

---
+++ Lab18.3/Money.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Money
+{
+public:
+	Money();
+	Money(unsigned long int R, unsigned int K);
+	Money(const Money& other);
+	~Money();
+	void show();
+	Money& operator = (const Money& other);
+	bool operator == (const Money& other);
+	bool operator != (const Money& other);
+	Money operator +(const Money& other);
+	Money operator -(const Money& other);
+	friend ostream& operator << (ostream& out, const Money& other);
+	friend istream& operator >> (istream& in, Money& other);
+private:
+	unsigned long int R;
+	unsigned int K;
+};
+
